d52a08c1f3b2605996186d3781f668dd8546ac1b
Anjali Arethiya <anjaliarethiya490@gmail.com>
2025-03-25 20:50:17 +0530

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; 
-import Home from './components/Home.jsx';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
+import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <Router>
+    <>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Home />
         <Footer />
       </div>
-    </Router>
+    </>
   );
 }
 
